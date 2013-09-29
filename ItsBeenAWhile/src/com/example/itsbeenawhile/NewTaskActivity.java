8dc6666a652cd5765fe9c9d1bee823b957b12950
Kyle Ong <ongman@berkeley.edu>
2013-09-28 17:26:40 -0700

    Made task-saving within NewTask

--- ItsBeenAWhile/src/com/example/itsbeenawhile/NewTaskActivity.java
+++ ItsBeenAWhile/src/com/example/itsbeenawhile/NewTaskActivity.java
@@ -1,14 +1,46 @@
 package com.example.itsbeenawhile;
 
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Fareki on 9/28/13.
+ * Hijacked by Ongman shortly after.
  */
 public class NewTaskActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newtask);
     }
+    
+    
+    
+    
+    
+    public void saveTask(View view) {
+    	try{
+    	String task = "task" + Calendar.getInstance().toString();
+    	byte[] dateBytes = new byte[5];
+    	dateBytes[0] = (byte) 2;
+    	dateBytes[1] = (byte) 2;
+    	dateBytes[2] = (byte) 2;
+    	dateBytes[3] = (byte) 2;
+    	
+    	File file = new File("data/data/com.example.itsbeenawhile/task.txt");
+    	FileOutputStream fos = getApplicationContext().openFileOutput(task+".txt", Context.MODE_PRIVATE);
+    	fos.write(dateBytes);
+    	fos.close();
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
 }
