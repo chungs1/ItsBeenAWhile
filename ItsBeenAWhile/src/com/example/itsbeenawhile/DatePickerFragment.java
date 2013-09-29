c6619084a24d05fa84ba63d565b49ac72b24f9df
Sophia Chung <chungs1@berkeley.edu>
2013-09-28 17:41:36 -0700

    making more android activities.

---
+++ ItsBeenAWhile/src/com/example/itsbeenawhile/DatePickerFragment.java
@@ -0,0 +1,13 @@
+package com.example.itsbeenawhile;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+/**
+ * Created by Fareki on 9/28/13.
+ */
+ public class DatePickerFragment extends {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+
+}
--- ItsBeenAWhile/src/com/example/itsbeenawhile/TimePickerFragment.java
+++ ItsBeenAWhile/src/com/example/itsbeenawhile/TimePickerFragment.java
@@ -1,13 +1,40 @@
 package com.example.itsbeenawhile;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager.*;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TimePicker;
+import java.util.Calendar;
 
 /**
  * Created by Fareki on 9/28/13.
  */
-public class TimePickerFragment extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        //super.onCreate(savedInstanceState);
+        final Calendar c = Calendar.getInstance();
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int year = c.get(Calendar.YEAR);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+
+    public void showTimeDatePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "timePicker");
     }
 }
